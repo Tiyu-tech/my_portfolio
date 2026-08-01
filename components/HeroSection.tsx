e026068a24b516340c39b1d880526234f647dba6
Tiyufx <tiyumbanazeeru@gmail.com>
2026-07-31 21:55:33 -0600

    Created Hero Section

---
+++ components/HeroSection.tsx
@@ -0,0 +1,11 @@
+function HeroSection() {
+  return (
+    <div className="flex font-extrabold">
+      <p className="hero-header text-6xl">
+        Hello my name is Tiyumba <i> from Tiyu Tech</i>
+      </p>
+    </div>
+  );
+}
+
+export default HeroSection;
